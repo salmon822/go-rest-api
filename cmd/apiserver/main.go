a09ef1a9d1831fe1920a3f43b39a958ab821107f
salmon882 <orunalievsolomonnn@gmail.com>
2023-07-19 13:03:03 +0600

    added new python file

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
-	"log"
+	"fmt"
 )
 
 func main() {
-	s := apiserver.New()
-	if err := s.Start(); err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println("Welcum here!")
+
+	var fruitList = []string{"Apple", "Tomato", "Peach"}
+	fmt.Printf("Type of fruitlist is %T/", fruitList)
+
+	fruitList = append(fruitList, "Mango", "Banana")
+
+	fmt.Println(fruitList)
+
+	fruitList = append(fruitList[1:3])
+
+	fmt.Println(fruitList)
 }
